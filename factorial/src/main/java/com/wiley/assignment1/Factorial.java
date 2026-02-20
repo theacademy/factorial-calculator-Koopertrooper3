6b7165bc907c0471a42d027bc87633c5f7174d78
Christian Yu <123125803+Koopertrooper3@users.noreply.github.com>
2026-02-19 23:38:49 -0500

    Commit1

--- factorial/src/main/java/com/wiley/assignment1/Factorial.java
+++ factorial/src/main/java/com/wiley/assignment1/Factorial.java
@@ -34,8 +34,18 @@ public class Factorial {
 		*/
 
 		//YOUR CODE STARTS HERE
+		if(!this.scanner.hasNextInt()){
+			this.printStream.print("Invalid entry. Please enter an integer between 1 and 10, inclusive.");
+			return -1;
+		}
 
-		return -1;
+		int input = this.scanner.nextInt();
+
+		if(input > 10 || input < 1){
+			this.printStream.print("Invalid entry. Please enter an integer between 1 and 10, inclusive.");
+			return -1;
+		}
+		return input;
 
 		//YOUR CODE ENDS HERE
 		 
@@ -53,7 +63,9 @@ public class Factorial {
 		 
 		int result = 1;
 		//YOUR CODE STARTS HERE
-
+		for(int i = num; i > 0; i--){
+			result = result * i;
+		 }
  
 
 		//YOUR CODE ENDS HERE
